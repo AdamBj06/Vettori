057d1ea0b066304fa0aab90e5eaa3d415a784707
Adam Ben jeddou <mcadambenjeddou@gmail.com>
2024-02-17 00:11:33 +0100

    Aggiungere i file di progetto.

---
+++ Vettori/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vettori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("massa in Kg: ");
+            double m = double.Parse(Console.ReadLine());
+            Console.WriteLine("accelerazione (x;y) in m/s^2: ");
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            Vettore a = new Vettore(x, y);
+            Console.WriteLine("F = " +  (m * a).ToString());
+
+            Console.Write("Posizione iniziale x in m (metri): ");
+            double s0x = double.Parse(Console.ReadLine());
+            Console.Write("Posizione iniziale y in m (metri): ");
+            double s0y = double.Parse(Console.ReadLine());
+            Console.WriteLine("velocità iniziale (x;y) in m/s: ");
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            Vettore v0 = new Vettore(x, y);
+            Console.WriteLine("tempo in s (secondi): ");
+            double t = double.Parse(Console.ReadLine());
+            Console.WriteLine("s = " + Math.Sqrt(Math.Pow(s0x + v0.X * t + 0.5f * a.X * t * t, 2) + Math.Pow(s0y + v0.Y * t + 0.5f * a.Y * t * t, 2)).ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vettori/Vettore.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vettori
+{
+    internal class Vettore
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vettore(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0};{1}", X, Y);
+        }
+
+        public static Vettore Parse(string str)
+        {
+            string[] substr = str.Split(';');
+            return new Vettore(double.Parse(substr[0]), double.Parse(substr[1]));
+        }
+
+        public static bool TryParse(string str, out Vettore v)
+        {
+            double x; double y;
+            string[] substr = str.Split(';');
+            if (substr.Length > 1 && double.TryParse(substr[0], out x) && double.TryParse(substr[1], out y))
+            {
+                v = new Vettore(x, y);
+                return true;
+            } 
+            else 
+            { 
+                v = null;
+                return false; 
+            }
+        }
+
+        public static double Modulo(Vettore v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y);
+        }
+
+        public static Vettore operator +(Vettore v1, Vettore v2)
+        {
+            return new Vettore(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vettore operator -(Vettore v1, Vettore v2)
+        {
+            return new Vettore(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vettore operator *(Vettore v1, Vettore v2)
+        {
+            return new Vettore(v1.X * v2.X, v1.Y * v2.Y);
+        }
+        public static Vettore operator *(Vettore v1, double s)
+        {
+            return new Vettore(v1.X * s, v1.Y * s);
+        }
+        public static Vettore operator *(double s, Vettore v2)
+        {
+            return new Vettore(s * v2.X, s * v2.Y);
+        }
+
+        public static Vettore operator /(Vettore v1, double s)
+        {
+            return new Vettore(v1.X / s, v1.Y / s);
+        }
+
+        public static bool operator ==(Vettore v1, Vettore v2)
+        {
+            if(v1.X == v2.X && v2.Y == v1.Y)
+            {
+                return true;
+            } 
+            else { return false; }
+        }
+
+        public static bool operator !=(Vettore v1, Vettore v2)
+        {
+            if (v1.X != v2.X || v2.Y != v1.Y)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
